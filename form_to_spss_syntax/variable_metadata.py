29ccd3c3707c0bd545c9f78c1e421e17492b9c36
Esmail Fadae <efadae@hotmail.com>
2014-06-14 21:42:14 -0400

    Initial working implementation.

--- form_to_spss_syntax/variable_metadata.py
+++ form_to_spss_syntax/variable_metadata.py
@@ -6,6 +6,7 @@ Created on Jun 13, 2014
 
 from collections import namedtuple
 import json
+import re
 
 class VariableMetadata(namedtuple('_VariableMetadata', 'name, label, value_mappings')):
     '''
@@ -28,15 +29,25 @@ class VariableMetadata(namedtuple('_VariableMetadata', 'name, label, value_mappi
         
         :rtype: tuple(str, str)
         '''
-                
-        variable_label_line= '/' + self.name + ' "' + self.label + '"'
+        
+        # TODO: Should labels be truncated to 116 characters?
+        
+        # Variable labels aren't always specified.
+        variable_label_line= '/' + self.name + ' "'
+        if self.label == None:
+            variable_label_line+= self.name + '"'
+        else:
+            variable_label_line+= self.label + '"'
         
         # There aren't always value labels to report.
         if self.value_mappings == None:
             value_label_line= None
         else:        
             value_label_line= '/' + self.name
-            for value_name, value_label in self.value_mappings.iteritems():
+            sorted_value_names= self.value_mappings.keys()
+            sorted_value_names.sort()
+            for value_name in sorted_value_names:
+                value_label= self.value_mappings[value_name]
                 value_label_line+= ' ' + value_name + ' "' + value_label + '"'
         
         return variable_label_line, value_label_line
@@ -60,7 +71,8 @@ class VariableMetadata(namedtuple('_VariableMetadata', 'name, label, value_mappi
         value_label_lines= list()
         for var_metadata in variable_metadata_list:
             var_label_line,  val_label_line= var_metadata._export_spss_syntax()
-            variable_label_lines.append(var_label_line)
+            if var_label_line != None:
+                variable_label_lines.append(var_label_line)
             if val_label_line != None:
                 value_label_lines.append(val_label_line)
         
@@ -91,27 +103,54 @@ class VariableMetadata(namedtuple('_VariableMetadata', 'name, label, value_mappi
         '''
         
         form_dict= json.loads(form_json)
+        return cls._import(form_dict)
+    
+    @classmethod
+    def _import(cls, form_dict):
+        '''
+        Where the actual importing work occurs. Takes a form as a dictionary and 
+        returns the appropriate :py:class:`VariableMetadata` objects.
+        
+        :param dict form_dict: The ODK Collect form parsed into a :py:class:`dict`.
+        :rtype: list(:py:class:`VariableMetadata`)
+        '''
+        
         form_variables= form_dict['children']
         variable_metadata_list= list()
         for form_var in form_variables:
-            var_name= form_var['name']
-            var_label= form_var['label']
+            if form_var['type'] == 'group':
+                # Recursively import groups.
+                group_variable_metadata_list= cls._import(form_var)
+                variable_metadata_list.extend(group_variable_metadata_list)
+                continue
+        
+            var_name= form_var['name'].encode('utf-8')
+            if 'label' not in form_var.keys():
+                var_label= None
+            else:
+                var_label= form_var['label'].encode('utf-8')
             
             # TODO: Find out multi-select "type" (e.g. "select multiple")
             if form_var['type'] in ['select one']:
                 value_mappings_list= form_var['children']
                 value_mappings= dict()
                 for mapping in value_mappings_list:
-                    val_name= mapping['name']
-                    val_label= mapping['label']
+                    val_name= mapping['name'].encode('utf-8')
+                    val_label= mapping['label'].encode('utf-8')
                     value_mappings[val_name]= val_label
             else:
                 value_mappings= None
             
             variable_metadata= cls(var_name, var_label, value_mappings)
             variable_metadata_list.append(variable_metadata)
-        
-        return variable_metadata_list
             
+            # TODO: Not really knowing the "calculate" syntax, this is likely very brittle.
+            if form_var['type'] == 'calculate':
+                calculation_string= form_var['bind']['calculate']
+                # Find the first substring of the form "'matched substring:"
+                calculated_var_name= re.match(r'''^.+'(.+):''', calculation_string).groups()[0].encode('utf-8')
+                calculated_variable_metadata= cls(calculated_var_name, calculated_var_name, None)
+                variable_metadata_list.append(calculated_variable_metadata)
 
-        
+        return variable_metadata_list
+    
